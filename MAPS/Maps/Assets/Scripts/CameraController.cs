b974eedafabd21ed80f256033aecf5c66e246543
MartinC <martin.canadasregueira@gmail.com>
2020-05-21 18:11:08 +0200

    commit de implementacion de mapa

---
+++ MAPS/Maps/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using TMPro;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //The object that camara will follow, in that case the character
+    public GameObject followTarget;
+    //The coordinates to follow the target
+    private UnityEngine.Vector3 targetPos;
+    //how fast the camera will go before the player 
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = new UnityEngine.Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        //this lines interpolates the camera where starts and where will it have to be
+        transform.position = UnityEngine.Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ MAPS/Maps/Assets/Scripts/Player2D.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Player2D : MonoBehaviour
+{
+    public float speed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveA();
+        moveW();
+        moveD();
+        moveS();
+    }
+    void moveA()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Animator>().SetBool("RunLeft", true);
+            transform.Translate(-speed, 0, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            GetComponent<Animator>().SetBool("RunLeft", false);
+        }
+    }
+    void moveW()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Animator>().SetBool("RunBack", true);
+            transform.Translate(0, speed, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            GetComponent<Animator>().SetBool("RunBack", false);
+        }
+    }
+    void moveD()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Animator>().SetBool("RunRight", true);
+            transform.Translate(speed, 0,0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            GetComponent<Animator>().SetBool("RunRight", false);
+        }
+    }
+    void moveS()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Animator>().SetBool("RunGo", true);
+            transform.Translate(0, -speed, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            GetComponent<Animator>().SetBool("RunGo", false);
+        }
+    }
+
+}
